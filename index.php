a537b279b6a41aa7067823c9837deaaa8c8833b9
KristinaSiavkinaite <43643033+KristinaSiavkinaite@users.noreply.github.com>
2018-09-27 16:16:05 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include('header.php');
+ ?>
+
+
+
+      <!-- start 2 container -->
+      <div class="container-fluid">
+        <!-- start 1 row -->
+        <div class="row aukstis-726">
+          <!-- start 1 column -->
+          <div class="col-md big-foto">
+          </div>
+          <!-- end 1 column -->
+        </div>
+        <!-- end 1 row -->
+      </div>
+      <!-- end 2 container -->
+
+
+
+
+      <?php
+      include('footer.php');
+       ?>
